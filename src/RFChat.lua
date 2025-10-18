63edf9d7ac8fa2318748f563e7cda0af08fd9a2e
opussf <github@zz9-za.com>
2025-10-18 13:33:25 -0700

    Version and stubs
    The time to repair the roof is when the sun is shining.

--- src/RFChat.lua
+++ src/RFChat.lua
@@ -7,23 +7,25 @@ function RF.InitChat()
 	BNSendWhisper = RF.BNSendWhisper
 end
 function RF.ReplaceMessage( msgIn )
-	-- search for and replace {RF} or {RF#nnnn} with a Random Fortune
-	--print( "msgIn: "..msgIn )
-	msgNew = nil
-	local tokenStart, tokenEnd, fortuneIdx, useLotto = strfind( msgIn, "{[rR][fF][#]?(%d*)[#]?([lL]?)}" )
-	if tokenStart then
-		useLotto = string.upper(useLotto) == "L"
-		local fortuneStr, fortuneIdx = RF.GetFortune( fortuneIdx )
-		local lottoStr = RF.MakeLuckyNumber()
-		--print( "useLotto: "..( useLotto and "yes" or "no" ) )
-		--print( "tokenStart: "..tokenStart )
-		--print( "tokenEnd: "..tokenEnd )
-		--print( "index: "..index )
-		msgNew = string.sub( msgIn, 1, tokenStart-1 )..
-				fortuneStr..(useLotto and " "..RF.MakeLuckyNumber() or "")..
-				string.sub( msgIn, tokenEnd+1 )
+	if msgIn then
+		-- search for and replace {RF} or {RF#nnnn} with a Random Fortune
+		--print( "msgIn: "..msgIn )
+		msgNew = nil
+		local tokenStart, tokenEnd, fortuneIdx, useLotto = strfind( msgIn, "{[rR][fF][#]?(%d*)[#]?([lL]?)}" )
+		if tokenStart then
+			useLotto = string.upper(useLotto) == "L"
+			local fortuneStr, fortuneIdx = RF.GetFortune( fortuneIdx )
+			local lottoStr = RF.MakeLuckyNumber()
+			--print( "useLotto: "..( useLotto and "yes" or "no" ) )
+			--print( "tokenStart: "..tokenStart )
+			--print( "tokenEnd: "..tokenEnd )
+			--print( "index: "..index )
+			msgNew = string.sub( msgIn, 1, tokenStart-1 )..
+					fortuneStr..(useLotto and " "..RF.MakeLuckyNumber() or "")..
+					string.sub( msgIn, tokenEnd+1 )
+		end
+		return( ( msgNew or msgIn ) )
 	end
-	return( ( msgNew or msgIn ) )
 end
 function RF.SendChatMessage( msgIn, system, language, channel )
 	RF.OriginalSendChatMessage( RF.ReplaceMessage( msgIn ), system, language, channel )
